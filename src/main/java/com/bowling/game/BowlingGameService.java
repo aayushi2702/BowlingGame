4870eaa396e0e5130e6da2472ba7fbd8ac7ba9e4
Ayushi <48720540+aayushi2702@users.noreply.github.com>
2022-11-21 19:33:54 +0530

    Feat: Create a test case that needs to Roll a bowl 20 times each as input in bowling game in which output is expected as 16(this scenario is called as spare in bowling game) and refactor a code according to all test cases.

--- src/main/java/com/bowling/game/BowlingGameService.java
+++ src/main/java/com/bowling/game/BowlingGameService.java
@@ -1,14 +1,31 @@
 package com.bowling.game;
 
 public class BowlingGameService {
-	private int score = 0;
+	private int roll = 0;
+	private int[] rolls = new int[21]; //
 
 	public void rollingABowl_InBowlingGameTest(int pinDows) {
-		score=score+pinDows;
+		rolls[roll++] = pinDows;
 	}
 
 	public int getScoreAfterBowlHits() {
+		int score = 0;
+		int curser = 0;
+		for (int frame = 0; frame < 10; frame++) {
+			if (isSpare(curser)) {
+				score += 10 + rolls[curser + 2];
+				curser += 2;
+			} else {
+				score += rolls[curser] + rolls[curser + 1];
+				curser += 2;
+			}
+		}
 		return score;
+	}
+
+	private boolean isSpare(int curser) {
+
+		return rolls[curser] + rolls[curser + 1] == 10;
 	}
 
 }
