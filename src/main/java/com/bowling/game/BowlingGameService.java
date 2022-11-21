1484b2621530e9a00cbad5f12125c5e78d1a66f2
Ayushi <48720540+aayushi2702@users.noreply.github.com>
2022-11-21 18:37:11 +0530

    Feat: Create a test case that needs to Roll a Gutter bowl in bowling game in which output is expected as 0

--- src/main/java/com/bowling/game/BowlingGameService.java
+++ src/main/java/com/bowling/game/BowlingGameService.java
@@ -3,8 +3,11 @@ package com.bowling.game;
 public class BowlingGameService {
 
 	public void rollingABowl_InBowlingGameTest(int pinDows) {
-		
-		
+
 	}
-	
+
+	public int getScoreAfterBowlHits() {
+		return 0;
+	}
+
 }
