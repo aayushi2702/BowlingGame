9386bdfcfd91781225014d9edd2f2d7fbd816686
Ayushi <48720540+aayushi2702@users.noreply.github.com>
2022-11-21 18:37:04 +0530

    Feat: Create a test case that needs to Roll a Gutter bowl in bowling game in which output is expected as 0

--- src/test/java/com/bowlingTest/game/BowlingGameServiceTests.java
+++ src/test/java/com/bowlingTest/game/BowlingGameServiceTests.java
@@ -1,10 +1,21 @@
 package com.bowlingTest.game;
 
-import org.junit.jupiter.api.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
 
 import com.bowling.game.BowlingGameService;
 
 public class BowlingGameServiceTests {
+
+	BowlingGameService bowlingGameService;
+
+	@Before
+	public void setUp() {
+		bowlingGameService = new BowlingGameService();
+	}
 
 	@Test
 	public void getABowlingGame() {
@@ -15,6 +26,16 @@ public class BowlingGameServiceTests {
 	public void getARollerBowlInBowlingGame() {
 		BowlingGameService bowlingGameService = new BowlingGameService();
 		bowlingGameService.rollingABowl_InBowlingGameTest(0);
+	}
+
+	@Test
+	public void getARollerBowlScoringAGutterBowlTests() {
+		BowlingGameService bowlingGameService = new BowlingGameService();
+		for (int i = 0; i < 20; i++) {
+			bowlingGameService.rollingABowl_InBowlingGameTest(0);
+
+		}
+		assertThat(bowlingGameService.getScoreAfterBowlHits(),is(0));
 	}
 
 }
