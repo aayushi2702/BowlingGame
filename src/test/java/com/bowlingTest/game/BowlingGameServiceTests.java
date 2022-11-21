8334ced58102f06b85be4c14ea99e2c471eab8f0
Ayushi <48720540+aayushi2702@users.noreply.github.com>
2022-11-21 17:36:44 +0530

    build : Added junit and spring boot starter and maven dependencies which needs to run this maven project

---
+++ src/test/java/com/bowlingTest/game/BowlingGameServiceTests.java
@@ -0,0 +1,15 @@
+package com.bowlingTest.game;
+
+import org.junit.jupiter.api.Test;
+
+import com.bowling.game.BowlingGameService;
+
+public class BowlingGameServiceTests {
+			
+	@Test
+	public void getABowlingGame() {
+		new  BowlingGameService();
+	}
+	
+
+}
